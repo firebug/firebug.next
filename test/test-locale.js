b0a56cc07ea35211f474b15eb856a522d03b1c0f
Jan Odvarko <odvarko@gmail.com>
2014-07-03 09:13:37 +0200

    Simple test for locale API (plural)

---
+++ test/test-locale.js
@@ -0,0 +1,20 @@
+/* See license.txt for terms of usage */
+
+"use strict";
+
+const { Trace } = require("./core/trace.js");
+const { Locale } = require("./core/locale.js");
+
+exports["test locale (sync)"] = function(assert) {
+  let text;
+
+  // 1 item
+  text = Locale.$STRP("firebug.storage.totalItems", [1]);
+  assert.ok(text == "1 item in Storage", "Singular must be used");
+
+  // 2 items
+  text = Locale.$STRP("firebug.storage.totalItems", [2]);
+  assert.ok(text == "2 items in Storage", "Plural must be used");
+};
+
+require("sdk/test").run(exports);
