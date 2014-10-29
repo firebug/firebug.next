4cb83beedf4b5dad26805b7307b7d4792a5d8a8c
Florent Fayolle <florent.fayolle69@gmail.com>
2014-10-29 22:00:24 +0100

    Issue #143: some more WIP

--- test/test-command-editor.js
+++ test/test-command-editor.js
@@ -23,7 +23,8 @@ exports["test Command Editor"] = function(assert, done) {
       let panel = toolbox.getCurrentPanel();
       let jsterm = panel._firebugPanelOverlay.getTerminal();
       jsterm.once("sidebar-created", () => {
-        // xxxFlorent: any sidebar looks to be null here, I have to use a timer...
+        // xxxFlorent: panel._firebugPanelOverlay.sidebar; is null. I have to use a timer...
+        // Is there any other way to get the sidebar object? (the setTimeout is not a viable solution I guess)
         setTimeout(() => {
           let sidebar = panel._firebugPanelOverlay.sidebar;
           if (!sidebar)
@@ -41,50 +42,36 @@ exports["test Command Editor"] = function(assert, done) {
     let iframe = sidePanel.querySelector(".iframe-commandEditor");
     let editorWin = XPCNativeWrapper.unwrap(iframe.contentWindow);
     let { editor } = editorWin;
-    console.log(editor);
+    doTests(editor, sidePanel);
   }, (error) => console.error(error));
-  // Open a new browser tab.
-  // xxxHonza: use proper test page URL FIX ME
-  var panelId = "";
-  /*
-  // Wait till the tab is loaded.
-  // xxxHonza: there is a lot of logs in the console:
-  // Bug 1022658 - Heavy logging in the console slows down unit test execution
-  var tabBrowser = getBrowserForTab(newTab);
-  function onPageLoad() {
-    tabBrowser.removeEventListener("load", onPageLoad, true);
-
-    var panelId = "dev-panel-firebug-nextgetfirebug-com-DOM";
-    var tool = browser.gDevTools.getToolDefinition(panelId);
-    assert.ok(tool, "The DOM tool must exist!");
-
-    // Get debugging target for the new tab.
-    let target = devtools.TargetFactory.forTab(newTab);
-
-    // Open toolbox with the Hello World panel selected
-    browser.gDevTools.showToolbox(target, panelId).then(function(toolbox) {
-      var panel = toolbox.getCurrentPanel();
-
-      assert.ok(panel.id == panelId, "DOM panel is loaded!");
-
-      closeTab(newTab);
-      done();
-    }).then(null, console.error);
-  }
-
-  tabBrowser.addEventListener("load", onPageLoad, true);*/
 };
 
-function selectSidePanel(sidePanelId, panelId, tab, browser) {
+function doTests(editor, sidePanel) {
+  var instructions = "a = \"no selection\";";
+  instructions += "window.a || \"selection\";";
+
+  var selectionStart = instructions.indexOf(";")+1;
+
+  // expected results:
+  var RES_NO_SELECTION = 'no selection';
+  var RES_SELECTION = 'selection';
+
+  editor.setSelection(selectionStart);
+
+
 }
 
-/*
-function selectPanel(panelId, tab, browser) {
-  if (!browser) {
-    browser = getMostRecentBrowserWindow();
+// Taken from test/test-console-clear.js
+function waitForMessage(panel, callback) {
+  let overlay = panel._firebugPanelOverlay;
+  let doc = overlay.getPanelDocument();
+  let log = doc.querySelector(".message[category=console] .console-string");
+  if (log) {
+    callback();
+    return;
   }
-  let target = targetFor(tab);
-  let toolbox = 
-}*/
+}
+
+
 
 require("sdk/test").run(exports);
