dfd15ef2d66e08d306107d59f4ed1a0d0a426454
Jan Odvarko <odvarko@gmail.com>
2014-10-31 11:33:42 +0100

    Fix locale test

--- test/test-locale.js
+++ test/test-locale.js
@@ -7,6 +7,10 @@ const { Locale } = require("../lib/core/locale.js");
 exports["test Locale module API"] = function(assert) {
   let text;
 
+  // xxxHonza: it would be better to have test-locale.properties file
+  // for this test, but registerStringBundle expects chrome URL...
+  Locale.registerStringBundle("chrome://firebug/locale/firebug.properties");
+
   // 1 item
   text = Locale.$STRP("firebug.storage.totalItems", [1]);
   assert.ok(text == "1 item in Storage", "Singular must be used");
