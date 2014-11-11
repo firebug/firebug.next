8c5c89c6e564e1e083c03db3fb2f553f35d6d830
Jan Odvarko <odvarko@gmail.com>
2014-11-11 17:13:37 +0100

    Test: simplify test for console clear (using new API)

--- test/test-console-clear.js
+++ test/test-console-clear.js
@@ -3,86 +3,46 @@
 "use strict";
 
 const { Cu } = require("chrome");
-const { loadFirebug } = require("./common.js");
-const { getMostRecentBrowserWindow } = require("sdk/window/utils");
-const { openTab, getBrowserForTab, closeTab } = require("sdk/tabs/utils");
-const { setTimeout } = require("sdk/timers");
-const { serve, host } = require("./httpd.js");
+const { openToolbox } = require("./common.js");
+const { waitForMessage } = require("./console.js");
 
-const { devtools } = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
-
+// Content of the test page (one console log is coming from the content).
 const content =
   "<html><head></head><body><script>" +
   "console.log('hello')" +
   "</script></body></html>";
 
 exports["test Console clear button"] = function(assert, done) {
-  let browser = getMostRecentBrowserWindow();
+  // Configuration flags for toolbox opening.
+  let config = {
+    panelId: "webconsole",
+    pageContent: content
+  };
 
-  let title = "testPage1";
-  let url = host + title + ".html";
-  let server = serve({ name: title, content: content });
+  // Start HTTP server, open new tab and the toolbox.
+  openToolbox(config).then(({toolbox, cleanUp, overlay}) => {
+    assert.ok(overlay, "The Console panel must be overlaid");
 
-  loadFirebug();
+    let doc = overlay.getPanelDocument();
+    let clearButton = doc.querySelector(".webconsole-clear-console-button");
+    assert.ok(clearButton, "The clear button must exist");
 
-  let newTab = openTab(browser, url, {
-    inBackground: false
-  });
+    // Wait for console log.
+    let config = {
+      cssSelector: ".message[category=console] .console-string"
+    };
 
-  let tabBrowser = getBrowserForTab(newTab);
-  function onPageLoad() {
-    tabBrowser.removeEventListener("load", onPageLoad, true);
+    waitForMessage(toolbox, config).then(result => {
+      assert.ok(true, "There must not be a log in the Console panel");
 
-    let panelId = "webconsole";
-    let tool = browser.gDevTools.getToolDefinition(panelId);
-    let target = devtools.TargetFactory.forTab(newTab);
+      overlay.clearConsole();
 
-    // Open toolbox with the Console panel selected.
-    browser.gDevTools.showToolbox(target, panelId).then(function(toolbox) {
-      let panel = toolbox.getCurrentPanel();
+      let log = doc.querySelector(config.cssSelector);
+      assert.ok(!log, "There must not be a log in the Console panel");
 
-      let overlay = panel._firebugPanelOverlay;
-      assert.ok(overlay, "The Console panel must be overlaid");
-
-      let doc = overlay.getPanelDocument();
-      let clearButton = doc.querySelector(".webconsole-clear-console-button");
-      assert.ok(clearButton, "The clear button must exist");
-
-      waitForMessage(panel, () => {
-        let log = doc.querySelector(".message[category=console] .console-string");
-        assert.ok(log, "There must be a log in the Console panel");
-
-        overlay.clearConsole();
-
-        log = doc.querySelector(".message[category=console] .console-string");
-        assert.ok(!log, "There must not be a log in the Console panel");
-
-        // Asynchronously clean up
-        setTimeout(() => {
-          server.stop(() => {
-            closeTab(newTab);
-            done();
-          });
-        })
-      });
+      cleanUp(done);
     });
-  }
-
-  tabBrowser.addEventListener("load", onPageLoad, true);
-};
-
-function waitForMessage(panel, callback) {
-  let overlay = panel._firebugPanelOverlay;
-  let doc = overlay.getPanelDocument();
-  let log = doc.querySelector(".message[category=console] .console-string");
-  if (log) {
-    callback();
-    return;
-  }
-
-  panel.hud.ui.once("new-messages", (event, messages) => {
-    callback();
   });
-}
+};
 
 require("sdk/test").run(exports);
